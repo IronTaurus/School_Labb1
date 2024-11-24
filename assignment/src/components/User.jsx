2b8dff7c119be4d436e20305a293522eb1083438
IronTaurus <irontauru@gmail.com>
2024-11-24 22:31:55 +0100

    added users

---
+++ assignment/src/components/User.jsx
@@ -0,0 +1,14 @@
+import { useState, createContext } from "react";
+const UserContext = createContext()
+
+const User = () => {
+  const [user, setUser] = useState("Användare 1");
+  return (
+    <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}!`}</h1>
+    </UserContext.Provider>
+  );
+}
+
+export default User;
+
--- assignment/src/pages/Info.jsx
+++ assignment/src/pages/Info.jsx
@@ -1,7 +1,10 @@
+import User from "../components/User";
 
 const Info = () => {
     return(
         <>
+            <User/>
+
             <p>Namn: Johan J</p>
             <p>Skola: IT Högskolan Distans</p>
             <p>Labb: 1</p>
